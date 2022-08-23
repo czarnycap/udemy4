c2cf5e6d781dcc50a2e4bd304573e310cc20b3ba
ian.oginski <ian.oginski@protonmail.com>
2022-08-23 13:35:27 +0200

    new file added

--- test1.py
+++ test1.py
@@ -1,2 +1,14 @@
 print('hello world')
-# commenting 
+# commenting
+var = 100
+def ff1():
+    print(globals())
+
+def ff2():
+    print (var + 2)
+
+def ff23():
+    print(var)
+
+ff2()
+ff23()
--- test2.py
+++ test2.py
@@ -1,1 +1,7 @@
-# new script
+moje_imie = "Adam"
+moj_wiek = 37
+moja_waga = 92.7
+
+print(moje_imie,"\n")
+print(moj_wiek/2)
+print(moja_waga/2)
