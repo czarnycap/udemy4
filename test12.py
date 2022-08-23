40d5b182b9eab950fdbd84b5d40143a9e11c8e01
ian.oginski <ian.oginski@protonmail.com>
2022-08-23 14:29:31 +0200

    new file added

---
+++ test12.py
@@ -0,0 +1,3 @@
+str = input('please provide first number: ')
+str2 = input('please provide second number: ')
+print(str,'+',str2,'=',int(str)+int(str2))
--- test2.py
+++
@@ -1,7 +0,0 @@
-moje_imie = "Adam"
-moj_wiek = 37
-moja_waga = 92.7
-
-print(moje_imie,"\n")
-print(moj_wiek*4)
-print(moja_waga)
