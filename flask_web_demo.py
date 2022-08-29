59158ddd2bdc430672f3e577c6ee0ef7ebccd775
ian.oginski <ian.oginski@protonmail.com>
2022-08-29 17:01:29 +0200

    added flash demo

--- flask_web_demo.py
+++ flask_web_demo.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+    print("get request string")
     return render_template("index.html")
-app.run(host="0.0.0.0")
 
+@app.route('/', methods = ['POST'])
+def home_post():
+    print("get post request")
+    return render_template("index.html")
+
+
+app.run(host="0.0.0.0")
